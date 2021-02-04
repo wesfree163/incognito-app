e7d537990f70557ee809ef86e7c628e498627466
wesfree163 <wesfree163@gmail.com>
2021-02-04 12:09:10 -0500

    published to Github Pages

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import {AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig)
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AmplifySignOut />
+        <h2>Incognito App</h2>
+      </header>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
