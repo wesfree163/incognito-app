65c711c2ff59e53ddb76e40cc81cde0bde90e638
wesfree163 <wesfree163@gmail.com>
2021-02-18 18:33:08 -0500

    Feb 18 Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import logo from './logo.svg';
 import Amplify from 'aws-amplify';
+import ilogo from './img/ilogo.png';
+import userI from './img/userIcon.png';
 import awsconfig from './aws-exports';
+import Button from 'react-bootstrap/Button';
+import SearchField from 'react-search-field';
 import {AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 
+
 Amplify.configure(awsconfig)
+
+const searchStyle = {
+  
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <AmplifySignOut />
+      <header className="App-header" style={{
+        backgroundColor: '#d8d8d8c4'
+      }}>
+        
         <h2>Incognito App</h2>
+        <div className="bar"> 
+          <img src={ilogo} className="logo" alt="inc-logo"/>
+          <Button className="Button">Home</Button>
+          <Button className="Button">Hierarchy</Button>
+          <Button className="Button">Add Field</Button>  
+        </div>
+
+        <div className="search">
+          <SearchField className style={{}}/>
+        </div>
+
+        <img src={userI} className="user" alt="inc-logo"/>
+
+        <AmplifySignOut className="AmplifySignOut"/>  
       </header>
     </div>
   );
